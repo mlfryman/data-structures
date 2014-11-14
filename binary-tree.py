256dd01a314f4baf761ba511270660f7da28a757
Melanie L. Fryman <melanie@frymanet.com>
2014-11-14 10:05:00 -0600

    save

--- binary-tree.py
+++ binary-tree.py
@@ -1,3 +1,67 @@
+### Chyld's solution ###
+class Node:
+    self.data = data
+    self.prev = None
+    self.next = None
+
+
+class BinaryTree:
+    root = None
+    def to_list(self):
+        list = []
+        if self.root:
+            self.visit_node(self.root, list)
+        return list
+
+    # recursive
+    def visit_node(self, node, list):
+        if node.prev:
+            self.visit_node(node.prev, list)
+        list.append(node.data)
+        if node.next:
+            self.visit_node(node.next, list)
+
+    # find returns the parent of the new node
+    def find(self, name, node = None):
+        if not node:
+            node = self.root
+        if name < node.data and node.prev:
+            return self.find(name, node.prev)
+        elif name > node.data and node.next:
+            return self.find(name, node.next)
+        else:
+            return node
+
+    def insert(self, name):
+        if not self.root:
+            self.root = Node(name)
+        else:
+            node = self.find(name)
+            if name < node.data:
+                node.prev = Node(name)
+            else:
+                node.next = Node(name)
+
+
+people = BinaryTree()
+people.insert("Mark")
+people.insert("Charlie")
+people.insert("Alex")
+people.insert("Donnie")
+people.insert("Bob")
+people.insert("Barnie")
+people.insert("Peter")
+people.insert("Sam")
+people.insert("Rex")
+people.insert("")
+
+people.to_list()
+
+
+### END Chyld's solution ###
+
+
+### My solution ###
 class Tree:
     """
     Tree node: left and right child + data, which can be any object
@@ -32,18 +96,19 @@ class Tree:
         Print tree path in order
         """
         if self.left:
-            self.left.printTree()
+            self.left.print_tree()
         print(self.data),
         if self.right:
-            self.right.printTree()
+            self.right.print_tree()
 
 # test binary tree
-tree = Tree("Rinne")
+tree = Tree("Trotz")
 tree.insert("Webber")
 tree.insert("Fisher")
+tree.insert("Rinne")
 tree.insert("Jones")
 tree.insert("Gausted")
 
-tree.printTree()
+tree.print_tree()
 
 pass
